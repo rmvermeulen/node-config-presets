9d9307e74437624e6dab3566aff391575ecf6c1f
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2018-12-29 17:43:34 +0100

    Add ls/list option

--- use-presets.js
+++ use-presets.js
@@ -1,33 +1,57 @@
 #! /usr/bin/env node
 // @ts-check
-const [...files] = process.argv.slice(2);
 
+const fs = require('fs');
+const { resolve, basename } = require('path');
+
+const presetDir = resolve(__dirname, 'presets/');
+const args = process.argv.slice(2);
+
+const handleError = (error, file) => {
+  console.error(`Ignoring ${file} (${error.code})`);
+};
+
+const handleResult = (cb = _ => {}) => (error, data) => {
+  if (error) {
+    return handleError(error);
+  }
+  return cb(data);
+};
+
+if (args.length === 0) {
+  process.exit();
+}
+
+const [cmd, ...tail] = args;
+
+// check for ls/list option
+if (cmd === 'ls' || cmd === 'list') {
+  fs.readdir(
+    presetDir,
+    handleResult(files => {
+      for (const file of files) {
+        console.log(file);
+      }
+      process.exit();
+    }),
+  );
+}
+
+const files = args;
 if (files.length) {
   // use __dirname to find the presets in node_modules
-  const { resolve, basename } = require('path');
   const filesAndPaths = files.map(file => ({
     file,
-    path: resolve(__dirname, 'presets/', file),
+    path: resolve(presetDir, file),
   }));
-
-  const handleError = (error, file) => {
-    console.error(`Ignoring ${file} (${error.code})`);
-  };
 
   // use cwd() to copy to caller's directory
   const outDir = process.cwd();
-  const { readFile, writeFile } = require('fs');
   for (const { file, path } of filesAndPaths) {
     const outPath = resolve(outDir, file);
-    readFile(path, (error, data) => {
-      if (error) {
-        return handleError(error, file);
-      }
-      writeFile(outPath, data, error => {
-        if (error) {
-          return handleError(error, basename(outPath));
-        }
-      });
-    });
+    fs.readFile(
+      path,
+      handleResult(data => fs.writeFile(outPath, data, handleResult())),
+    );
   }
 }
